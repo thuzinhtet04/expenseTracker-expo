d76cc69b5c7de434eddc16e087422aa1134ad293
thuzinhtet <thuzinhtet.dev@gmail.com>
2025-10-07 19:29:13 +0630

    write css & apply using StyleSheet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text , StyleSheet } from 'react-native'
 import React from 'react'
 
 const app = () => {
   return (
-    <View>
-      <Text>Coffee shop</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Coffee shop</Text>
     </View>
   )
 }
 
-export default app
+export default app;
+
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+    flexDirection : "column",
+
+  },
+  text : {
+    color : 'white',
+    fontSize : 22,
+    fontWeight : "bold",
+    textAlign :"center"
+  }
+})
